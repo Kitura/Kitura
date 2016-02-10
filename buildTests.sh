10aa26710dcfec90ef6eb9d3fa5f33ecffccd2e3
Robert F. Dickerson <rfdickerson@gmail.com>
2016-02-10 15:24:27 -0600

    Test case scripts added

---
+++ buildTests.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#/**
+#* Copyright IBM Corporation 2016
+#*
+#* Licensed under the Apache License, Version 2.0 (the "License");
+#* you may not use this file except in compliance with the License.
+#* You may obtain a copy of the License at
+#*
+#* http://www.apache.org/licenses/LICENSE-2.0
+#*
+#* Unless required by applicable law or agreed to in writing, software
+#* distributed under the License is distributed on an "AS IS" BASIS,
+#* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#* See the License for the specific language governing permissions and
+#* limitations under the License.
+#**/
+
+SCRIPT_DIR=$(dirname "$BASH_SOURCE")
+cd "$SCRIPT_DIR"
+
+if [ -f "./mainBuildTests.sh" ]; then
+    if [ -x "./mainBuildTests.sh" ]; then
+        ./mainBuildTests.sh
+    else
+        echo "Main test builder script isn't executable"
+        exit 1
+    fi
+else
+    fwDir="!"
+    for  dir in Packages/PhoenixTestFramework* ; do
+        fwDir=$dir
+    done
+    if [[ "${fwDir}" !=  "!"  &&  -d "${fwDir}" ]]; then
+        "${fwDir}/TestFramework/mainBuildTests.sh"
+    else
+        echo "Didn't find the Phoenix test framework"
+        exit 1
+    fi
+fi
