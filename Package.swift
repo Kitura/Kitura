c0891e64c5475f5382087dad95a7f1be21368fb6
rolivieri <r_olivieri@yahoo.com>
2016-02-10 14:13:29 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,8 @@ import PackageDescription
 
 // Dual pathing for O/S differences
 #if os(Linux)
-   let swiftyJsonUrl = "https://github.com/IBM-Swift/SwiftyJSON.git"
+   //let swiftyJsonUrl = "https://github.com/IBM-Swift/SwiftyJSON.git"
+   let swiftyJsonUrl = "git@github.com:IBM-Swift/SwiftyJSON.git"
    let swiftyJsonVersion = 3
 #else
    let swiftyJsonUrl = "https://github.com/SwiftyJSON/SwiftyJSON.git"
@@ -28,11 +29,16 @@ import PackageDescription
 let package = Package(
     name: "router",
         dependencies: [
-            .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0),
-            .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 0),
-            .Package(url: "https://github.com/IBM-Swift/Kitura-Pcre2.git", majorVersion: 0),
-            .Package(url: "https://github.com/IBM-Swift/Kitura-CurlHelpers.git", majorVersion: 0),
-            .Package(url: "https://github.com/IBM-Swift/Kitura-HttpParserHelper.git", majorVersion: 0),
+            //.Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0),
+            .Package(url: "git@github.com:IBM-Swift/Kitura-net.git", majorVersion: 0),
+            //.Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 0),
+            .Package(url: "git@github.com:IBM-Swift/LoggerAPI.git", majorVersion: 0),
+            //.Package(url: "https://github.com/IBM-Swift/Kitura-Pcre2.git", majorVersion: 0),
+            .Package(url: "git@github.com:IBM-Swift/Kitura-Pcre2.git", majorVersion: 0),
+            //.Package(url: "https://github.com/IBM-Swift/Kitura-CurlHelpers.git", majorVersion: 0),
+            .Package(url: "git@github.com:IBM-Swift/Kitura-CurlHelpers.git", majorVersion: 0),
+            //.Package(url: "https://github.com/IBM-Swift/Kitura-HttpParserHelper.git", majorVersion: 0),
+            .Package(url: "git@github.com:IBM-Swift/Kitura-HttpParserHelper.git", majorVersion: 0),
             .Package(url: swiftyJsonUrl, majorVersion: swiftyJsonVersion)
         ]
 )
