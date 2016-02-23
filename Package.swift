0dd2a070edb34e5302bdd8760388b4e050f2adea
Vadim Eisenberg <vadime@il.ibm.com>
2016-02-23 20:43:24 +0200

    IBM-Swift/Kitura#128 removed commented out code from Package.swift

--- Package.swift
+++ Package.swift
@@ -25,17 +25,12 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura-router.git", versions: Version(0,2,0)..<Version(0,3,0)),
-        //.Package(url: "git@github.com:IBM-Swift/Kitura-router.git", majorVersion: 0),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", versions: Version(0,2,0)..<Version(0,3,0)),
-        //.Package(url: "git@github.com:IBM-Swift/HeliumLogger.git", versions: Version(0,0,0)..<Version(0,1,0)),
         .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", versions: Version(0,2,0)..<Version(0,3,0)),
-        //.Package(url: "git@github.com:IBM-Swift/LoggerAPI.git", majorVersion: 0),
         .Package(url: "https://github.com/IBM-Swift/Kitura-TestFramework.git", versions: Version(0,2,0)..<Version(0,3,0)),
-        //.Package(url: "git@github.com:IBM-Swift/Kitura-TestFramework.git", majorVersion: 0),
         ]
 )
 
 #if os(OSX)
-    //package.dependencies.append(.Package(url: "git@github.com:IBM-Swift/GRMustache.swift.git", majorVersion: 1))
     package.dependencies.append(.Package(url: "https://github.com/IBM-Swift/GRMustache.swift.git", majorVersion: 1))
 #endif
