ed704639742d3861a34c6d84a03fb249d50ab45a
Robert F. Dickerson <rfdickerson@gmail.com>
2016-02-28 10:00:30 -0600

    Using develop version of HeliumLogger

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura-router.git", versions: Version(0,3,0)..<Version(0,4,0)),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", versions: Version(0,2,0)..<Version(0,3,0)),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", versions: Version(0,3,0)..<Version(0,4,0)),
         .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", versions: Version(0,3,0)..<Version(0,4,0)),
         .Package(url: "https://github.com/IBM-Swift/Kitura-TestFramework.git", versions: Version(0,3,0)..<Version(0,4,0)),
     ]
