2f3daddff15676fdd9a2c2b8dd36f122059d262d
Ira Rosen <IRAR@il.ibm.com>
2016-03-09 15:11:38 +0200

    Update Kitura-net version

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura-Router",
         dependencies: [
-            .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 4),
+            .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 5),
             .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 0, minor: 4),
             .Package(url: "https://github.com/IBM-Swift/Kitura-CurlHelpers.git", majorVersion: 0, minor: 4),
             .Package(url: "https://github.com/IBM-Swift/Kitura-HttpParserHelper.git", majorVersion: 0, minor: 4),
