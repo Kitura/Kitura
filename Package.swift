584e81458ce6821305f925d7dfc1c194385832d9
Daniel Firsht <dfirsht@us.ibm.com>
2016-03-09 17:16:20 -0600

    #242 added Kitura-Sample as a test dependency

--- Package.swift
+++ Package.swift
@@ -34,5 +34,8 @@ let package = Package(
             .Package(url: "https://github.com/IBM-Swift/Kitura-HttpParserHelper.git", majorVersion: 0, minor: 4),
             .Package(url: swiftyJsonUrl, majorVersion: swiftyJsonVersion),
             .Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 0, minor: 0)
+        ],
+        testDependencies: [
+            .Package(url: "https://github.com/IBM-Swift/Kitura-Sample.git", majorVersion: 0, minor: 5)
         ]
 )
