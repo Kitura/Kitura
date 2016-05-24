3297207d60a13ae289f5744c887855a24fa117d8
Ira Rosen <IRAR@il.ibm.com>
2016-05-24 13:41:07 +0300

    Update Kitura-net version

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura",
         dependencies: [
-            .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 13),
+            .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 15),
             .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 7),
             .Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 0, minor: 13)
         ]
