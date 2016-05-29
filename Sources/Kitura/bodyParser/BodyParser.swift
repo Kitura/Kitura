3fa2cfb717d64f7e71f45b99ff5a910337259814
dsperling <dsperling@smithmicro.com>
2016-05-29 12:56:36 -0700

    Fix for issue #474

--- Sources/Kitura/bodyParser/BodyParser.swift
+++ Sources/Kitura/bodyParser/BodyParser.swift
@@ -71,10 +71,16 @@ public class BodyParser: RouterMiddleware {
     ///
     public class func parse(_ message: SocketReader, contentType: String?) -> ParsedBody? {
 
-        guard let contentType = contentType else {
+        guard var contentType = contentType else {
             return nil
         }
         
+        // Handle Content-Type with parameters.  For example, treat:
+        // "application/x-www-form-urlencoded; charset=UTF-8" as
+        // "application/x-www-form-urlencoded"
+        if let parameterStart = contentType.range(of: ";") {
+            contentType = contentType.substring(to: parameterStart.lowerBound)
+        }
         if let parser = getParsingFunction(contentType: contentType) {
             return parse(message, parser: parser)
         }
