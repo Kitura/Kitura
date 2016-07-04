42ed431bb11dbfc15fb9b274e08e98e8eb1dff02
Shmuel Kallner <kallner@il.ibm.com>
2016-07-04 19:41:16 +0300

    IBM-Swift/Kitura3417 Updated the minor of KituraNet

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura",
         dependencies: [
-            .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 20),
+            .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 21),
             .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 9),
             .Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 0, minor: 16)
         ]
