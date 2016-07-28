314a301c96bf2a0f5ffc9f14a744290dcfd3eb05
Vadim Eisenberg <vadime@il.ibm.com>
2016-07-28 19:53:53 +0300

    IBM-Swift/Kitura#611 fixed Package.swift
    to the up-todate versions
    removed redundant dependencies statement

--- Package.swift
+++ Package.swift
@@ -18,15 +18,10 @@ import PackageDescription
 
 let package = Package(
     name: "Kitura",
-        dependencies: [
-            .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 21),
-            .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 10, minor: 0),
-            .Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 0, minor: 16)
-        ]
     dependencies: [
-            .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 17),
-            .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 8),
-            .Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 0, minor: 13)
+        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 23),
+        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 10, minor: 0),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 0, minor: 16)
     ],
     exclude: ["Configuration", "Scripts"]
 )
