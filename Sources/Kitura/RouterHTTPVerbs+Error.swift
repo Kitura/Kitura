b5201eca1dc29eb0f6250028ebd05e93a5f119af
Vadim Eisenberg <vadime@il.ibm.com>
2016-07-28 20:02:51 +0300

    IBM-Swift/Kitura#611 removed error verb from the generated files
    added Sources/Kitura/RouterHTTPVerbs+Error.swift file

---
+++ Sources/Kitura/RouterHTTPVerbs+Error.swift
@@ -0,0 +1,40 @@
+/*
+ * Copyright IBM Corporation 2015
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// MARK Router
+
+extension Router {
+    // MARK: error
+    @discardableResult
+    public func error(_ handler: RouterHandler...) -> Router {
+        return routingHelper(.error, pattern: nil, handler: handler)
+    }
+
+    @discardableResult
+    public func error(_ handler: [RouterHandler]) -> Router {
+        return routingHelper(.error, pattern: nil, handler: handler)
+    }
+
+    @discardableResult
+    public func error(_ middleware: RouterMiddleware...) -> Router {
+        return routingHelper(.error, pattern: nil, middleware: middleware)
+    }
+
+    @discardableResult
+    public func error(_ middleware: [RouterMiddleware]) -> Router {
+        return routingHelper(.error, pattern: nil, middleware: middleware)
+    }
+}
