7b61a56cefdf9e1619f2f2728dcc4d1c1beb8204
Navneet Gupta <navneet@us.ibm.com>
2016-09-09 19:12:19 -0500

    Only run xcodebuild on Darwin

--- xcodebuild.sh
+++ xcodebuild.sh
@@ -3,6 +3,12 @@
 SCHEME=Kitura
 SDK=macosx10.12
 
+OS=`uname`
+if [[ $OS != "Darwin" ]]; then
+    echo "Skipping xcodebuild as not available on ${OS}"
+    exit 0
+fi
+
 swift package generate-xcodeproj
 PROJECT="${SCHEME}.xcodeproj"
 
@@ -12,9 +18,9 @@ which -s xcpretty
 XCPRETTY_INSTALLED=$?
 
 if [[ $TRAVIS || $XCPRETTY_INSTALLED == 0 ]]; then
-  echo "Piping ${TEST_CMD} to xcpretty"
+  echo "Running ${TEST_CMD} | xcpretty"
   eval "${TEST_CMD} | xcpretty"
 else
   echo "xcpretty not installed. Running ${TEST_CMD}"
-  eval "$TEST_CMD"
+  eval "${TEST_CMD}"
 fi
