d7806ec6a87086cdcef2c18a3fbc35096016c9a0
Navneet Gupta <navneet@us.ibm.com>
2016-09-29 16:03:43 -0500

    xcodebuild workaround and fix test causing crash

--- xcodebuild.sh
+++ xcodebuild.sh
@@ -1,32 +1,23 @@
 #! /bin/bash
 
 SCHEME=Kitura
-SDK=macosx10.12
 
 OS=`uname`
 if [[ $OS == "Darwin" ]]; then
-    echo "Starting xcodebuild on ${OS} (TRAVIS_OS_NAME: ${TRAVIS_OS_NAME}) using SDK: ${SDK}"
+    XCODEBUILD_VERSION=`xcodebuild -version`
+    echo "Starting xcodebuild (${XCODEBUILD_VERSION}) on ${OS}"
 else
-    echo "Skipping xcodebuild as not available on ${OS} (TRAVIS_OS_NAME: ${TRAVIS_OS_NAME})"
+    echo "Skipping xcodebuild as not available on ${OS}"
     exit 0
 fi
 
 swift package generate-xcodeproj
 PROJECT="${SCHEME}.xcodeproj"
 
-TEST_CMD="xcodebuild -scheme $SCHEME -project $PROJECT -sdk $SDK -destination 'arch=x86_64' -enableCodeCoverage YES test"
+TEST_CMD="xcodebuild -project $PROJECT -scheme $SCHEME -sdk macosx -enableCodeCoverage YES test"
+echo "Running ${TEST_CMD}"
+eval "${TEST_CMD}"
 
-which -s xcpretty
-XCPRETTY_INSTALLED=$?
-
-if [[ $TRAVIS || $XCPRETTY_INSTALLED == 0 ]]; then
-  echo "Running ${TEST_CMD} | xcpretty"
-  eval "${TEST_CMD} | xcpretty"
-else
-  echo "xcpretty not installed. Running ${TEST_CMD}"
-  eval "${TEST_CMD}"
-fi
-
-bash <(curl -s https://codecov.io/bash)
+bash <(curl -s https://codecov.io/bash) -J "^${SCHEME}\$"
 
 echo "Finished xcodebuild on ${OS}"
