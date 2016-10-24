c6d994e868534fc6c630b57efa7f45f14e42f69b
Youming Lin <youminglin@outlook.com>
2016-10-24 11:51:43 -0500

    Add more unit tests

--- Tests/KituraTests/TestRouteRegex.swift
+++ Tests/KituraTests/TestRouteRegex.swift
@@ -34,21 +34,36 @@ class TestRouteRegex: XCTestCase {
             var regex: NSRegularExpression?
         #endif
         var strings: [String]?
+        var path: String
+        var range: NSRange
 
         // Partial match false adds '$' end of string special character
         (regex, strings) = RouteRegex.sharedInstance.buildRegex(fromPattern: "test", allowPartialMatch: false)
+        path = "/test"
+        range = regex!.rangeOfFirstMatch(in: path, range: NSMakeRange(0, path.characters.count))
         XCTAssertEqual(regex!.pattern, "^/test/?$")
         XCTAssertTrue(strings!.isEmpty)
+        XCTAssertEqual(range.location, 0)
+        XCTAssertEqual(range.length, 5)
+
 
         // Partial match true does not include '$' end of string special character
         (regex, strings) = RouteRegex.sharedInstance.buildRegex(fromPattern: "test", allowPartialMatch: true)
+        path = "/test/hello/world"
+        range = regex!.rangeOfFirstMatch(in: path, range: NSMakeRange(0, path.characters.count))
         XCTAssertEqual(regex!.pattern, "^/test/?(?=/|$)")
         XCTAssertTrue(strings!.isEmpty)
+        XCTAssertEqual(range.location, 0)
+        XCTAssertEqual(range.length, 5)
 
         (regex, strings) = RouteRegex.sharedInstance.buildRegex(fromPattern: "test/:id", allowPartialMatch: true)
+        path = "/test/123/hello/world"
+        range = regex!.rangeOfFirstMatch(in: path, range: NSMakeRange(0, path.characters.count))
         XCTAssertEqual(regex!.pattern, "^/test/(?:([^/]+?))/?(?=/|$)")
         XCTAssertFalse(strings!.isEmpty)
         XCTAssertEqual(strings![0], "id")
+        XCTAssertEqual(range.location, 0)
+        XCTAssertEqual(range.length, 9)
 
         (regex, strings) = RouteRegex.sharedInstance.buildRegex(fromPattern: "test/:id+", allowPartialMatch: false)
         XCTAssertEqual(regex!.pattern, "^/test/([^/]+?(?:/[^/]+?)*)/?$")
