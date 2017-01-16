b6d5e7578e8247984bb13b02c98c5d04f73f2da5
Shmuel Kallner <kallner@il.ibm.com>
2017-01-16 17:12:00 +0200

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -19,9 +19,9 @@ import PackageDescription
 let package = Package(
     name: "Kitura",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 4),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 5),
         .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 1, minor: 4)
+        .Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 1, minor: 5)
     ],
     exclude: ["Configuration", "Scripts"]
 )
