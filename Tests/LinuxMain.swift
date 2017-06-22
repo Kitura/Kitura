58c2abecb49399d0427e8084ecca3b17cd594bf9
Quan Vo <quan.vo@ibm.com>
2017-06-22 10:45:17 -0500

    Linux tests array shuffle fixes for Swift 4

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -18,13 +18,16 @@ import XCTest
 import Glibc
 @testable import KituraTests
 
+srand(UInt32(time(nil)))
+
 // http://stackoverflow.com/questions/24026510/how-do-i-shuffle-an-array-in-swift
+
+#if !swift(>=3.2)
 extension MutableCollection where Indices.Iterator.Element == Index {
     mutating func shuffle() {
         let c = count
         guard c > 1 else { return }
-        
-        srand(UInt32(time(nil)))
+
         for (firstUnshuffled , unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
             let d: IndexDistance = numericCast(random() % numericCast(unshuffledCount))
             guard d != 0 else { continue }
@@ -33,6 +36,21 @@ extension MutableCollection where Indices.Iterator.Element == Index {
         }
     }
 }
+#else
+extension MutableCollection {
+    mutating func shuffle() {
+        let c = count
+        guard c > 1 else { return }
+
+        for (firstUnshuffled , unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+            let d: IndexDistance = numericCast(random() % numericCast(unshuffledCount))
+            guard d != 0 else { continue }
+            let i = index(firstUnshuffled, offsetBy: d)
+            self.swapAt(firstUnshuffled, i)
+        }
+    }
+}
+#endif
 
 extension Sequence {
     func shuffled() -> [Iterator.Element] {
@@ -56,4 +74,4 @@ XCTMain([
     testCase(TestSubrouter.allTests.shuffled()),
     testCase(TestStaticFileServer.allTests.shuffled()),
     testCase(TestTemplateEngine.allTests.shuffled())
-].shuffled())
+    ].shuffled())
