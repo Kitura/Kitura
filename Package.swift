3c9b9d82672e54b050ff3efee804da02d875c4b0
Carl Brown <carlb@pobox.com>
2017-07-17 18:03:51 -0500

    Incorporate BlueSocket for Swift4

--- Package.swift
+++ Package.swift
@@ -15,17 +15,16 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/IBM-Swift/CHTTPParser.git", .branch("master")),
-        .package(url: "https://github.com/swift-server/http.git", .branch("develop")),
+        .package(url: "https://github.com/carlbrown/http.git", .branch("swift4")),
         .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", from: "1.7.0"),
         .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.7.0"),
-        .package(url: "https://github.com/IBM-Swift/BlueSocket.git", from: "0.12.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Kitura",
-            dependencies: ["CHTTPParser","SwiftServerHTTP","HeliumLogger","LoggerAPI","Socket"]),
+            dependencies: ["CHTTPParser","HTTP","HeliumLogger","LoggerAPI"]),
         .testTarget(
             name: "KituraTests",
             dependencies: ["Kitura"]),
