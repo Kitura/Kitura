ff9c56420a4aaea9d3e7a781f2c8c4c0399f4e8c
Mike Tunnicliffe <mike.tunnicliffe@uk.ibm.com>
2017-10-19 16:23:21 +0000

    Expose document root dir used by StaticFileServer

--- Sources/Kitura/staticFileServer/StaticFileServer.swift
+++ Sources/Kitura/staticFileServer/StaticFileServer.swift
@@ -70,6 +70,8 @@ public class StaticFileServer: RouterMiddleware {
         }
     }
 
+    public let absoluteRootPath: String
+
     let fileServer: FileServer
 
     /// Initializes a `StaticFileServer` instance.
@@ -81,7 +83,7 @@ public class StaticFileServer: RouterMiddleware {
     /// the headers of the response.
     public init(path: String = "./public", options: Options = Options(),
                  customResponseHeadersSetter: ResponseHeadersSetter? = nil) {
-        let path = StaticFileServer.ResourcePathHandler.getAbsolutePath(for: path)
+        absoluteRootPath = StaticFileServer.ResourcePathHandler.getAbsolutePath(for: path)
 
         let cacheOptions = options.cacheOptions
         let cacheRelatedHeadersSetter =
@@ -92,7 +94,7 @@ public class StaticFileServer: RouterMiddleware {
         let responseHeadersSetter = CompositeRelatedHeadersSetter(setters: cacheRelatedHeadersSetter,
                                                                   customResponseHeadersSetter)
 
-        fileServer = FileServer(servingFilesPath: path, options: options,
+        fileServer = FileServer(servingFilesPath: absoluteRootPath, options: options,
                                 responseHeadersSetter: responseHeadersSetter)
     }
 
