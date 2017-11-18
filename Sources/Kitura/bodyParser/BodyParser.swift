82e1d3a7777bc7d387a618c5e58e9d3b64d38b61
lroseblade <lroseblade@uk.ibm.com>
2017-11-18 15:42:01 +0530

    Correct typo in BodyParser API ref

--- Sources/Kitura/bodyParser/BodyParser.swift
+++ Sources/Kitura/bodyParser/BodyParser.swift
@@ -21,7 +21,7 @@ import Foundation
 
 // MARK: BodyParser
 
-/// The BodyParser parses the body of the request prior to sending it to the handler. It reads the Content-Type of in the message header and populates the `RouterRequest` body field with a `ParsedBody` enumeration (e.g. json, raw, text, urlEncoded)
+/// The BodyParser parses the body of the request prior to sending it to the handler. It reads the Content-Type of the message header and populates the `RouterRequest` body field with a `ParsedBody` enumeration (e.g. json, raw, text, urlEncoded)
 /// In order for the BodyParser to be used it must first be registered with any routes that are interested in the ParsedBody payload. The example below registers all routes to the BodyParser middleware.
 ///```swift
 ///   router.all("/*", middleware: BodyParser())
