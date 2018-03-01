e35e2c7f2c5a62f1aaed929cc567356622140e7b
Helen Masters <helen_postlethwaite@uk.ibm.com>
2018-03-01 16:07:38 +0000

    Update SSLConfig API doc (#1224)

--- Sources/Kitura/SSLConfig.swift
+++ Sources/Kitura/SSLConfig.swift
@@ -17,52 +17,65 @@
 import SSLService
 
 // MARK: SSLConfig
-/// SSLConfig
+/// A struct that allows you to configure your SSL using a CA certificate file (Linux), a CA certificate directory (Linux) or a certificate chain file (MacOS).
 public struct SSLConfig {
 
-    // MARK: Properties
-    /// Config required to configure the `SSLService`
+    /// :nodoc:
     public private(set) var config: SSLService.Configuration
 
     // MARK: Lifecycle
 
     #if os(Linux)
-    //MARK: Linux
-    /// Initialize an SSLService.Configuration instance using a `CA Certificate` file.
+    //MARK: For Linux
+    /// Initialize an `SSLService.Configuration` instance using a CA certificate file.
     ///
     /// - Parameter caCertificateFilePath: Path to the PEM formatted CA certificate file.
     /// - Parameter certificateFilePath: Path to the PEM formatted certificate file.
     /// - Parameter keyFilePath: Path to the PEM formatted key file. If nil, `certificateFilePath` will be used.
-    /// - Parameter selfSigned:	True if certs are `self-signed`, false otherwise. Defaults to true.
-    ///	- Returns:	New SSLConfig instance.
+    /// - Parameter selfSigned:	True if certs are *self-signed*, false otherwise. Defaults to true.
+    /// - Parameter cipherSuite: Unused.
+    ///	- Returns:	New `SSLConfig` instance.
     public init(withCACertificateFilePath caCertificateFilePath: String?, usingCertificateFile certificateFilePath: String?, withKeyFile keyFilePath: String? = nil, usingSelfSignedCerts selfSigned: Bool = true, cipherSuite: String? = nil) {
 
         config = SSLService.Configuration(withCACertificateFilePath: caCertificateFilePath, usingCertificateFile: certificateFilePath, withKeyFile:keyFilePath, usingSelfSignedCerts: selfSigned, cipherSuite: cipherSuite)
     }
 
-    /// Initialize an SSLService.Configuration instance using a `CA Certificate` directory.
+    /// Initialize an `SSLService.Configuration` instance using a CA certificate directory.
     ///
-    ///	*Note:* `caCertificateDirPath` - All certificates in the specified directory **must** be hashed using the `OpenSSL Certificate Tool`.
+    ///	*Note:* `caCertificateDirPath` - all certificates in the specified directory **must** be hashed using the OpenSSL Certificate Tool.
     ///
     /// - Parameter caCertificateDirPath: Path to a directory containing CA certificates. *(see note above)*
     /// - Parameter certificateFilePath: Path to the PEM formatted certificate file. If nil, `certificateFilePath` will be used.
     /// - Parameter keyFilePath: Path to the PEM formatted key file (optional). If nil, `certificateFilePath` is used.
-    /// - Parameter selfSigned:	True if certs are `self-signed`, false otherwise. Defaults to true.
-    ///	- Returns: New SSLConfig instance.
+    /// - Parameter selfSigned:	True if certs are *self-signed*, false otherwise. Defaults to true.
+    /// - Parameter cipherSuite: Unused.
+    ///	- Returns: New `SSLConfig` instance.
     public init(withCACertificateDirectory caCertificateDirPath: String?, usingCertificateFile certificateFilePath: String?, withKeyFile keyFilePath: String? = nil, usingSelfSignedCerts selfSigned: Bool = true, cipherSuite: String? = nil) {
 
         config = SSLService.Configuration(withCACertificateDirectory:caCertificateDirPath, usingCertificateFile: certificateFilePath, withKeyFile: keyFilePath, usingSelfSignedCerts: selfSigned, cipherSuite: cipherSuite)
     }
     #endif // os(Linux)
-    //MARK: MacOS
-    /// Initialize an SSLService.Configuration instance using a `Certificate Chain File`.
+    //MARK: For MacOS
+    /// Initialize an `SSLService.Configuration` instance using a certificate chain file.
     ///
     /// *Note:* If using a certificate chain file, the certificates must be in PEM format and must be sorted starting with the subject's certificate (actual client or server certificate), followed by intermediate CA certificates if applicable, and ending at the highest level (root) CA.
     ///
-    /// - Parameter chainFilePath: Path to the certificate chain file (optional). *(see note above)*
+    /// For testing purposes you will most likely want to create and use some self-signed certificates. Follow the
+    /// instructions in our [Enabling SSL/TLS On Your Kitura Server](http://www.kitura.io/en/resources/tutorials/ssl.html) tutorial.
+    /// ### Usage Example: ###
+    /// This example initializes an `SSLConfig` instance and then associates this SSL configuration with the Kitura HTTP
+    /// server registration.
+    /// ```swift
+    /// let mySSLConfig =  SSLConfig(withChainFilePath: "/tmp/Creds/Self-Signed/cert.pfx",
+    ///                              withPassword: "password",
+    ///                              usingSelfSignedCerts: true)
+    /// Kitura.addHTTPServer(onPort: 8080, with: router, withSSL: mySSLConfig)
+    /// ```
+    /// - Parameter chainFilePath: Path to the certificate chain file (optional). *(See note above)*
     /// - Parameter password: Export password for the chain file (optional). This is required if using a certificate chain file.
-    /// - Parameter selfSigned:	True if certs are `self-signed`, false otherwise. Defaults to true.
-    ///	- Returns:	New SSLConfig instance.
+    /// - Parameter selfSigned:	True if certs are *self-signed*, false otherwise. Defaults to true.
+    /// - Parameter cipherSuite: Unused.
+    ///	- Returns:	New `SSLConfig` instance.
     public init(withChainFilePath chainFilePath: String? = nil, withPassword password: String? = nil, usingSelfSignedCerts selfSigned: Bool = true, cipherSuite: String? = nil) {
 
         config = SSLService.Configuration(withChainFilePath: chainFilePath, withPassword: password, usingSelfSignedCerts: selfSigned, cipherSuite: cipherSuite)
