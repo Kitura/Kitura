49ace673382d978afa6e359a5d078285c7d6e6cc
David Jones <djones6dev@gmail.com>
2018-05-16 13:47:47 +0100

    Revert "Relax Codable Router type requirements" (#1269)
    This reverts #1242 (commit 91f47de6251b20f5dfb7cfdb4319bb19cbf37291).

--- Package.swift
+++ Package.swift
@@ -31,7 +31,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/Kitura-net.git", from: "2.1.0"),
         .package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", from: "1.7.4"),
-        .package(url: "https://github.com/IBM-Swift/KituraContracts.git", from: "0.0.23")
+        .package(url: "https://github.com/IBM-Swift/KituraContracts.git", from: "0.0.24"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
