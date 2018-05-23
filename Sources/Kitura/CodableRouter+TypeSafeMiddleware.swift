160322160ce6432be17043260b5704dab9c179ac
andy <andrew.lees11@ibm.com>
2018-05-23 09:26:50 +0100

    removed typeAliases

--- Sources/Kitura/CodableRouter+TypeSafeMiddleware.swift
+++ Sources/Kitura/CodableRouter+TypeSafeMiddleware.swift
@@ -24,69 +24,6 @@ import Dispatch
 
 extension Router {
 
-    // Used by PUT and PATCH with identifier
-    public typealias MiddlewareIdentifierCodableClosure<T: TypeSafeMiddleware, Id: Identifier, I: Codable, O: Codable> = (T, Id, I, @escaping CodableResultClosure<O>) -> Void
-    
-    public typealias TwoMiddlewareIdentifierCodableClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware, Id: Identifier, I: Codable, O: Codable> = (T1, T2, Id, I, @escaping CodableResultClosure<O>) -> Void
-    
-    public typealias ThreeMiddlewareIdentifierCodableClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware, T3: TypeSafeMiddleware, Id: Identifier, I: Codable, O: Codable> = (T1, T2, T3, Id, I, @escaping CodableResultClosure<O>) -> Void
-    
-    // Used by POST
-    public typealias MiddlewareCodableClosure<T: TypeSafeMiddleware, I: Codable, O: Codable> = (T, I, @escaping CodableResultClosure<O>) -> Void
-    
-    public typealias TwoMiddlewareCodableClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware, I: Codable, O: Codable> = (T1, T2, I, @escaping CodableResultClosure<O>) -> Void
-    
-    public typealias ThreeMiddlewareCodableClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware, T3: TypeSafeMiddleware, I: Codable, O: Codable> = (T1, T2, T3, I, @escaping CodableResultClosure<O>) -> Void
-
-    // Used by POST with identifier
-    public typealias MiddlewareCodableIdentifierClosure<T: TypeSafeMiddleware, I: Codable, Id: Identifier, O: Codable> = (T, I, @escaping IdentifierCodableResultClosure<Id, O>) -> Void
-    
-    public typealias TwoMiddlewareCodableIdentifierClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware, I: Codable, Id: Identifier, O: Codable> = (T1, T2, I, @escaping IdentifierCodableResultClosure<Id, O>) -> Void
-    
-    public typealias ThreeMiddlewareCodableIdentifierClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware, T3: TypeSafeMiddleware, I: Codable, Id: Identifier, O: Codable> = (T1, T2, T3, I, @escaping IdentifierCodableResultClosure<Id, O>) -> Void
-    
-    // Used by DELETE
-    public typealias MiddlewareNonCodableClosure<T: TypeSafeMiddleware> = (T, @escaping ResultClosure) -> Void
-
-    public typealias TwoMiddlewareNonCodableClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware> = (T1, T2, @escaping ResultClosure) -> Void
-
-    public typealias ThreeMiddlewareNonCodableClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware, T3: TypeSafeMiddleware> = (T1, T2, T3, @escaping ResultClosure) -> Void
-
-    // Used by DELETE with identifier
-    public typealias MiddlewareIdentifierNonCodableClosure<T: TypeSafeMiddleware, Id: Identifier> = (T, Id, @escaping ResultClosure) -> Void
-
-    public typealias TwoMiddlewareIdentifierNonCodableClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware, Id: Identifier> = (T1, T2, Id, @escaping ResultClosure) -> Void
-
-    public typealias ThreeMiddlewareIdentifierNonCodableClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware, T3: TypeSafeMiddleware, Id: Identifier> = (T1, T2, T3, Id, @escaping ResultClosure) -> Void
-
-    // Used by GET returning array
-    public typealias MiddlewareCodableArrayClosure<T: TypeSafeMiddleware, O: Codable> = (T, @escaping CodableArrayResultClosure<O>) -> Void
-    
-    public typealias TwoMiddlewareCodableArrayClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware, O: Codable> = (T1, T2, @escaping CodableArrayResultClosure<O>) -> Void
-
-    public typealias ThreeMiddlewareCodableArrayClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware, T3: TypeSafeMiddleware, O: Codable> = (T1, T2, T3, @escaping CodableArrayResultClosure<O>) -> Void
-
-    // Used by GET with identifier returning array
-    public typealias MiddlewareIdentifierCodableArrayClosure<T: TypeSafeMiddleware, Id: Identifier, O: Codable> = (T, @escaping IdentifierCodableArrayResultClosure<Id, O>) -> Void
-    
-    public typealias TwoMiddlewareIdentifierCodableArrayClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware, Id: Identifier, O: Codable> = (T1, T2, @escaping IdentifierCodableArrayResultClosure<Id, O>) -> Void
-
-    public typealias ThreeMiddlewareIdentifierCodableArrayClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware, T3: TypeSafeMiddleware, Id: Identifier, O: Codable> = (T1, T2, T3, @escaping IdentifierCodableArrayResultClosure<Id, O>) -> Void
-
-    // Used by GET returning single codable
-    public typealias MiddlewareSimpleCodableClosure<T: TypeSafeMiddleware, O: Codable> = (T, @escaping CodableResultClosure<O>) -> Void
-    
-    public typealias TwoMiddlewareSimpleCodableClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware, O: Codable> = (T1, T2, @escaping CodableResultClosure<O>) -> Void
-
-    public typealias ThreeMiddlewareSimpleCodableClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware, T3: TypeSafeMiddleware, O: Codable> = (T1, T2, T3, @escaping CodableResultClosure<O>) -> Void
-
-    // Used by GET with identifier returning single codable
-    public typealias MiddlewareIdentifierSimpleCodableClosure<T: TypeSafeMiddleware, Id: Identifier, O: Codable> = (T, Id, @escaping CodableResultClosure<O>) -> Void
-    
-    public typealias TwoMiddlewareIdentifierSimpleCodableClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware, Id: Identifier, O: Codable> = (T1, T2, Id, @escaping CodableResultClosure<O>) -> Void
-
-    public typealias ThreeMiddlewareIdentifierSimpleCodableClosure<T1: TypeSafeMiddleware, T2: TypeSafeMiddleware, T3: TypeSafeMiddleware, Id: Identifier, O: Codable> = (T1, T2, T3, Id, @escaping CodableResultClosure<O>) -> Void
-    
     // GET returning single Codable
     public func get<T: TypeSafeMiddleware, O: Codable>(_ route: String, handler: @escaping MiddlewareSimpleCodableClosure<T, O>) {
         get(route) { request, response, next in
