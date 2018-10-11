ea8567a48b6039d2c819bb8ff9d84778c068bfee
David Jones <djones6dev@gmail.com>
2018-10-11 16:18:30 +0100

    Improve logging for SwaggerGenerator (#1339)

--- Package.swift
+++ Package.swift
@@ -41,7 +41,7 @@ let package = Package(
         kituraNetPackage,
         .package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", from: "2.0.0"),
         .package(url: "https://github.com/IBM-Swift/KituraContracts.git", from: "1.0.0"),
-        .package(url: "https://github.com/IBM-Swift/TypeDecoder.git", from: "1.1.0")
+        .package(url: "https://github.com/IBM-Swift/TypeDecoder.git", from: "1.3.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
