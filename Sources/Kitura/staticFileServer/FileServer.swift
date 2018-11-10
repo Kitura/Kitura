e2697e9de49d73b216f8e5fced3fe9a60cf226fd
Caleb Kussmaul <calebwk@gmail.com>
2018-11-10 08:32:09 -0500

    Implemented HTTP Not Modified response for conditional-GET using etags and fixed documentation issue where max-age is listed as milliseconds instead of seconds

--- Sources/Kitura/staticFileServer/FileServer.swift
+++ Sources/Kitura/staticFileServer/FileServer.swift
@@ -187,9 +187,15 @@ extension StaticFileServer {
                         // Send only headers
                         _ = response.send(status: .OK)
                     } else {
-                        // Send the entire file
-                        try response.send(fileName: filePath)
-                        response.statusCode = .OK
+                        if let etag = request.headers["If-None-Match"],
+                          etag == CacheRelatedHeadersSetter.calculateETag(from: fileAttributes) {
+                            response.statusCode = .notModified
+                            response.send("")
+                        } else {
+                            // Send the entire file
+                            try response.send(fileName: filePath)
+                            response.statusCode = .OK
+                        }
                     }
                 }
             } catch {
