02d8e278f44d34c39a5a0bc88916077c73155304
David Jones <djones6@uk.ibm.com>
2019-04-04 11:03:46 +0100

    Update Kitura-net dependency

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ var kituraNetPackage: Package.Dependency
 if ProcessInfo.processInfo.environment["KITURA_NIO"] != nil {
     kituraNetPackage = .package(url: "https://github.com/IBM-Swift/Kitura-NIO.git", from: "2.0.0")
 } else {
-    kituraNetPackage = .package(url: "https://github.com/IBM-Swift/Kitura-net.git", from: "2.1.0")
+    kituraNetPackage = .package(url: "https://github.com/IBM-Swift/Kitura-net.git", from: "2.2.0")
 }
 
 let package = Package(
