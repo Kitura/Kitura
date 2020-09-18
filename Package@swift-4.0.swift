fb699e715a513805165437eac8fe8086fe75f175
Fouad Hatem <fouadhatem@icloud.com>
2020-09-18 16:59:57 +0300

    Update package references
    Update package references from IBM-Swift to Kitura

--- Package@swift-4.0.swift
+++ Package@swift-4.0.swift
@@ -1,21 +1,5 @@
 // swift-tools-version:4.0
 
-/**
- * Copyright IBM Corporation 2016, 2017
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
-
 import PackageDescription
 import Foundation
 
@@ -28,12 +12,12 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", from: "1.9.0"),
+        .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "1.9.0"),
         .package(url: "https://github.com/apple/swift-log.git", Version("0.0.0") ..< Version("2.0.0")),
-        .package(url: "https://github.com/IBM-Swift/Kitura-net.git", from: "2.4.0"),
-        .package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", from: "2.0.0"),
-        .package(url: "https://github.com/IBM-Swift/KituraContracts.git", from: "1.0.0"),
-        .package(url: "https://github.com/IBM-Swift/TypeDecoder.git", from: "1.3.0"),
+        .package(url: "https://github.com/Kitura/Kitura-net.git", from: "2.4.0"),
+        .package(url: "https://github.com/Kitura/Kitura-TemplateEngine.git", from: "2.0.0"),
+        .package(url: "https://github.com/Kitura/KituraContracts.git", from: "1.0.0"),
+        .package(url: "https://github.com/Kitura/TypeDecoder.git", from: "1.3.0"),
     ],
     targets: [
         .target(
