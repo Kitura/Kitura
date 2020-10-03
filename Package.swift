f45f2d35e4d60f2ec480c6c6fe91e9a1c6406dd4
Danny Sung <danny@dannysung.com>
2020-10-03 16:32:52 -0700

    Update package reference to community versions

--- Package.swift
+++ Package.swift
@@ -36,12 +36,12 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "1.9.0"),
+        .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "1.9.200"),
         .package(url: "https://github.com/apple/swift-log.git", Version("0.0.0") ..< Version("2.0.0")),
         kituraNetPackage,
-        .package(url: "https://github.com/Kitura/Kitura-TemplateEngine.git", from: "2.0.0"),
-        .package(url: "https://github.com/Kitura/KituraContracts.git", from: "1.0.0"),
-        .package(url: "https://github.com/Kitura/TypeDecoder.git", from: "1.3.0"),
+        .package(url: "https://github.com/Kitura/Kitura-TemplateEngine.git", from: "2.0.200"),
+        .package(url: "https://github.com/Kitura/KituraContracts.git", from: "1.0.200"),
+        .package(url: "https://github.com/Kitura/TypeDecoder.git", from: "1.3.200"),
     ],
     targets: [
         .target(
