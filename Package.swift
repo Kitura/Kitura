2170106a745456b73c6f742a7002b60a46b28e0b
Danny Sung <danny@dannysung.com>
2020-10-04 09:18:52 -0700

    Update Package references for Kitura-net and Kitura-NIO to community versions

--- Package.swift
+++ Package.swift
@@ -22,9 +22,9 @@ import Foundation
 var kituraNetPackage: Package.Dependency
 
 if ProcessInfo.processInfo.environment["KITURA_NIO"] != nil {
-    kituraNetPackage = .package(url: "https://github.com/Kitura/Kitura-NIO.git", from: "2.3.0")
+    kituraNetPackage = .package(url: "https://github.com/Kitura/Kitura-NIO.git", from: "2.4.200")
 } else {
-    kituraNetPackage = .package(url: "https://github.com/Kitura/Kitura-net.git", from: "2.4.0")
+    kituraNetPackage = .package(url: "https://github.com/Kitura/Kitura-net.git", from: "2.4.200")
 }
 
 let package = Package(
