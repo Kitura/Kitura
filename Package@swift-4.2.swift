daf6479097469a0c1fe64755db6e23f788a3ec29
Danny Sung <danny@dannysung.com>
2020-10-04 10:58:12 -0700

    Update package reference to community versions (#1528)
    * Update package reference to community versions
    
    * Update Package references for Kitura-net and Kitura-NIO to community versions

--- Package@swift-4.2.swift
+++ Package@swift-4.2.swift
@@ -28,12 +28,12 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "1.9.0"),
+        .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "1.9.200"),
         .package(url: "https://github.com/apple/swift-log.git", Version("0.0.0") ..< Version("2.0.0")),
-        .package(url: "https://github.com/Kitura/Kitura-net.git", from: "2.4.0"),
-        .package(url: "https://github.com/Kitura/Kitura-TemplateEngine.git", from: "2.0.0"),
-        .package(url: "https://github.com/Kitura/KituraContracts.git", from: "1.0.0"),
-        .package(url: "https://github.com/Kitura/TypeDecoder.git", from: "1.3.0"),
+        .package(url: "https://github.com/Kitura/Kitura-net.git", from: "2.4.200"),
+        .package(url: "https://github.com/Kitura/Kitura-TemplateEngine.git", from: "2.0.200"),
+        .package(url: "https://github.com/Kitura/KituraContracts.git", from: "1.2.200"),
+        .package(url: "https://github.com/Kitura/TypeDecoder.git", from: "1.3.200"),
     ],
     targets: [
         .target(
